1773321f76894a1a48ccf5cc2d9cd412c2888c0a
24danielcr <24danielcr@gmail.com>
2025-06-18 00:33:47 -0600

    Pruebas Selenium Web Driver. Entrega 2 Laboratorio 6.

--- Laboratorio3_C23310/frontend-lab/src/components/PaisForm.vue
+++ Laboratorio3_C23310/frontend-lab/src/components/PaisForm.vue
@@ -1,50 +1,53 @@
-
+﻿
 <template>
     <div><h1>Formulario para crear un pais</h1></div>
     <div class="d-flex justify-content-center align-items-center vh-100">
-        <div class="card p-4 shadow" style="max-width: 400px; width: 100%">
-            <h3 class="text-center">
-                Formulario de creacion de paises
-            </h3>
-            <form @submit.prevent="guardarPais">
-                <div class="form-group">
-                    <label for="nombre">Nombre:</label>
-                    <input v-model="datosFormulario.nombre"
-                           type="text"
-                           id="name"
-                           class="form-control"
-                           required />
-                </div>
-                <div class="form-group">
-                    <label for="continente">Continente:</label>
-                    <select v-model="datosFormulario.continente"
-                            id="continente"
-                            required
-                            class="form-control">
-                        <option value="" disabled>Seleccione un continente </option>
-                        <option>Africa</option>
-                        <option>Asia</option>
-                        <option>Europa</option>
-                        <option>America</option>
-                        <option>Oceania</option>
-                        <option>Antartida</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="idioma">Idioma:</label>
-                    <input v-model="datosFormulario.idioma"
-                           type="text"
-                           id="idioma"
-                           class="form-control"
-                           required />
-                </div>
-                <div>
-                    <button type="submit" class="btn btn-success btn-block">
-                        Guardar
-                    </button>
-                </div>
-            </form>
+      <div class="card p-4 shadow" style="max-width: 400px; width: 100%">
+        <h3 class="text-center">
+          Formulario de creacion de paises
+        </h3>
+        <form @submit.prevent="guardarPais">
+          <div class="form-group">
+            <label for="nombre">Nombre:</label>
+            <input v-model="datosFormulario.nombre"
+                   type="text"
+                   id="name"
+                   class="form-control"
+                   required />
+          </div>
+          <div class="form-group">
+            <label for="continente">Continente:</label>
+            <select v-model="datosFormulario.continente"
+                    id="continente"
+                    required
+                    class="form-control">
+              <option value="" disabled>Seleccione un continente </option>
+              <option>Africa</option>
+              <option>Asia</option>
+              <option>Europa</option>
+              <option>America</option>
+              <option>Oceania</option>
+              <option>Antartida</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="idioma">Idioma:</label>
+            <input v-model="datosFormulario.idioma"
+                   type="text"
+                   id="idioma"
+                   class="form-control"
+                   required />
+          </div>
+          <div>
+            <button type="submit" class="btn btn-success btn-block">
+              Guardar
+            </button>
+          </div>
+        </form>
+        <div v-if="mostrarMensaje" class="form-control mt-3" readonly>
+          {{ mensaje }}
         </div>
+      </div>
     </div>
 </template>
 
@@ -53,21 +56,32 @@
     export default {
         data() {
             return {
-                datosFormulario: { nombre: "", continente: "", idioma: "" },
+              datosFormulario: { nombre: "", continente: "", idioma: "" },
+              mensaje: "",
+              mostrarMensaje: false,
             };
         },
         methods: {
             guardarPais() {
-                console.log("Datos a guardar:", this.datosFormulario);
+                this.mostrarMensaje = false;
+                this.mensaje = "";
                 axios.post("https://localhost:7093/api/Paises", {
                     nombre: this.datosFormulario.nombre,
                     continente: this.datosFormulario.continente,
                     idioma: this.datosFormulario.idioma,
-                }).then(function (response) {
-                    console.log(response);
-                    window.location.href = "/";
-                }).catch(function (error) {
-                    console.log(error);
+                }).then((response) => {
+                  console.log(response);
+                  if (response != null && response.data) {
+                    this.mensaje = "Pais guardado exitosamente!";
+                    this.mostrarMensaje = true;
+                  } else {
+                    this.mensaje = "No se pudo agregar pais. Intente mas tarde.";
+                    this.mostrarMensaje = true;
+                  }
+                }).catch((error) => {
+                  this.mensaje = "No se pudo agregar pais. Intente mas tarde.";
+                  this.mostrarMensaje = true;
+                  console.error(error);
                 });
             },
         },
---
+++ Laboratorio6_C23310/UIAutomationTests/UIAutomationTests/Selenium.cs
@@ -0,0 +1,122 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V135.Audits;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace UIAutomationTests
+{
+  public class Selenium
+  {
+    EdgeDriver driver;
+    [SetUp]
+    public void Setup()
+    {
+      driver = new EdgeDriver();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      driver.Dispose();
+    }
+
+    [Test]
+    public void Enter_To_List_Of_Countries_Test()
+    {
+      var URL = "http://localhost:8080/";
+      var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+      driver.Manage().Window.Maximize();
+      driver.Navigate().GoToUrl(URL);
+
+      int rowCount = 0;
+      try
+      {
+        var table = wait.Until(driver => driver.FindElement(By.CssSelector(
+          "#app > div > table")));
+        var rows = table.FindElements(By.TagName("tr"));
+        rowCount = rows.Count;
+      }
+      catch (NoSuchElementException ex)
+      {
+        Assert.Fail("Table not found: " + ex.Message);
+      }
+
+      try
+      {
+        var addCountryButton = wait.Until(ExpectedConditions
+          .ElementToBeClickable(By.CssSelector("#app > div > div > div > a >" +
+          " button")));
+        addCountryButton.Click();
+
+        var country = "Australia";
+        var nameInput = wait.Until(ExpectedConditions.ElementIsVisible(
+          By.Id("name")));
+        nameInput.SendKeys(country);
+
+        var option = 6; // Oceania
+        var continentSelect = wait.Until(driver =>
+            driver.FindElement(By.CssSelector("#continente > option:nth-child("
+              + option + ")")));
+        continentSelect.Click();
+
+        var language = "Inglés";
+        var languageSelect = driver.FindElement(By.Id("idioma"));
+        languageSelect.SendKeys(language);
+
+        var confirmButton = driver.FindElement(By.CssSelector(
+            "#app > div.d-flex.justify-content-center.align-items-center.vh-100"
+              + " > div > form > div:nth-child(4) > button"));
+        confirmButton.Click();
+      }
+      catch (Exception ex)
+      {
+        Assert.Fail("Form failed: " + ex.Message);
+      }
+
+      bool messageDisplayed = false;
+      string? messageText = "";
+
+      try
+      {
+        var messageBox = wait.Until(ExpectedConditions.ElementIsVisible(
+            By.CssSelector("#app > div.d-flex.justify-content-center." +
+              "align-items-center.vh-100 > div > div")));
+
+        messageDisplayed = messageBox.Displayed;
+        messageText = messageBox.Text;
+      }
+      catch (WebDriverTimeoutException ex)
+      {
+        Assert.Fail("Could not add country: " + ex.Message);
+      }
+
+      driver.Navigate().GoToUrl(URL);
+
+      int rowCountUpdated = 0;
+      try
+      {
+        var table = wait.Until(driver => driver.FindElement(By.CssSelector(
+          "#app > div > table")));
+        var rows = table.FindElements(By.TagName("tr"));
+        rowCountUpdated = rows.Count;
+      }
+      catch (NoSuchElementException ex)
+      {
+        Assert.Fail("Table not found: " + ex.Message);
+      }
+
+      if (messageText == null)
+      {
+        messageText = "";
+      }
+
+      Assert.That(messageDisplayed, Is.True);
+      Assert.That(messageText.Contains("Pais guardado exitosamente!"), Is.True);
+      Assert.Greater(rowCountUpdated, rowCount);
+    }
+  }
+}
